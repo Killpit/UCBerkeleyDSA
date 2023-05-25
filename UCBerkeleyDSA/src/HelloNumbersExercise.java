b220019c9c2a9d09fd71e0bbd2ecdcedb938d733
Ata Tekeli <tekeliata2000@gmail.com>
2023-05-25 12:40:14 +0300

    Add files via upload

---
+++ UCBerkeleyDSA/src/HelloNumbersExercise.java
@@ -0,0 +1,9 @@
+public class HelloNumbersExercise {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x <= 45) {
+            System.out.println(x);
+            x = x + 9;
+        }
+    }
+}
---
+++ UCBerkeleyDSA/src/helloNumbersExperiment.java
@@ -0,0 +1,6 @@
+public class helloNumbersExperiment {
+    public static void main(String[] args) {
+        String x = 5 + "10";
+        System.out.println(x);
+    }
+}
